ca328b87aadc218163aaacfaf3a3ddf726fc0c5b
gautamapp <30464574+gautamnimmala@users.noreply.github.com>
2021-05-31 17:05:23 +0530

    Update nginxwebserver.sh

--- nginxwebserver.sh
+++ nginxwebserver.sh
@@ -1,26 +1,32 @@
 #!/bin/bash -x
-${SSH_K8S-MASTER_NODE} 'pwd; ls'
-${SSH_K8S-MASTER_NODE} 'git clone https://github.com/gautamnimmala/Jenkins.git'
-${SSH_K8S-MASTER_NODE} 'cd Jenkins'
-${SSH_K8S-MASTER_NODE} 'docker build -f Dockerfile.nginx -t nginxweb:1.1 .'
-${SSH_K8S-MASTER_NODE} 'docker images'
-${SSH_K8S-MASTER_NODE} 'kubectl get nodes'
-nodes_status= `${SSH_K8S-MASTER_NODE} 'kubectl get nodes' |grep k8s-master`
-if [ "$nodes_status" == "NotReady"]
+${SSH_K8SMASTER_NODE} 'pwd; ls'
+${SSH_K8SMASTER_NODE} 'git clone https://github.com/gautamnimmala/Jenkins.git'
+sleep 20s
+${SSH_K8SMASTER_NODE} 'cd Jenkins; pwd; ls'
+${SSH_K8SMASTER_NODE} 'cd Jenkins; docker build -f Dockerfile.k8snginx -t k8snginx:1.1 .'
+${SSH_K8SMASTER_NODE} 'docker images'
+${SSH_K8SMASTER_NODE} 'kubectl get nodes'
+nodes_status= `${SSH_K8SMASTER_NODE} 'kubectl get nodes ' | awk '{print $2}' |grep Ready`
+if [ "$nodes_status" == "Ready"]
 then
-    ${SSH_K8S-MASTER_NODE} 'export kubever=$(kubectl version | base64 | tr -d '\n')'
-    ${SSH_K8S-MASTER_NODE} 'kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"'
-else 
     echo "The k8smaster node is in Ready state"
-sleep 10s    
-${SSH_K8S-MASTER_NODE} 'helm install nginxweb k8snginxst/ --values k8snginxst/values.yaml'
+else    
+    ${SSH_K8SMASTER_NODE} 'export kubever=$(kubectl version | base64 | tr -d '\n')'
+    ${SSH_K8SMASTER_NODE} 'kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"'
+fi   
+sleep 20s    
+${SSH_K8SMASTER_NODE} 'cd Jenkins; helm install nginxweb k8snginxst/ --values k8snginxst/values.yaml'
 sleep 30s
-${SSH_K8S-MASTER_NODE} 'kubectl get pods'
-pod_status= `${SSH_K8S-MASTER_NODE} 'kubectl get pods' | grep nginxweb`
-if [ "$pod_status" == "Pending"]
+${SSH_K8SMASTER_NODE} 'kubectl get pods'
+pod_status= `${SSH_K8SMASTER_NODE} 'kubectl get pods'  |awk '{print $4}' |grep Running`
+if [ "$pod_status" == "Running"]
 then
-    ${SSH_K8S-MASTER_NODE} 'kubectl taint nodes  k8s-master node-role.kubernetes.io/master-'
-else
     echo "The NGINX POD is in Running state"
+ else    
+    ${SSH_K8SMASTER_NODE} 'kubectl taint nodes  k8s-master node-role.kubernetes.io/master-'
+sleep 20s
+${SSH_K8SMASTER_NODE} 'kubectl get pods'
+
+    
 fi
 exit
